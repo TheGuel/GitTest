b538c91395c7b3a6ed01f911821268c38e735b8d
TheGuel <migreyes2001@gmail.com>
2025-02-06 14:12:03 -0500

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 0;
+
+        if (x < 0) {
+            System.out.println("Boom");
+        } else {
+            System.out.println("What?");
+        }
+    }
+}
